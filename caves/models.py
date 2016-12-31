806bc773bf23afa1b3359ebbdfaf5bb8182b5f5c
James Brill <jebssnooker@hotmail.com>
2016-12-31 16:10:06 +0000

    Add author to caves

--- caves/models.py
+++ caves/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Cave(models.Model):
     text = models.TextField()
+    author = models.ForeignKey(User, default=1)
