3e6e295dbc05bf53b87f88e3da19054fc4182770
James Brill <jebssnooker@hotmail.com>
2016-12-31 20:43:15 +0000

    Add endpoint for getting logged-in user's caves

--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from rest_framework import routers
 from rest_framework_jwt.views import verify_jwt_token
 from authentication.views import UserViewSet, get_token
-from caves.views import CaveView
+from caves.views import CaveView, get_my_caves
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -14,5 +14,6 @@ urlpatterns = [
     url(r'^api/auth/', get_token),
     url(r'^api/caves/$', CaveView.as_view()),
     url(r'^api/caves/(?P<uuid>[0-9a-z\-]+)/$', CaveView.as_view()),
+    url(r'^api/my-caves/$', get_my_caves),
     url(r'^api-token-verify/', verify_jwt_token),
 ]
--- caves/views.py
+++ caves/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.decorators import api_view
 from .models import Cave
 from .serializers import CaveSerializer
 from django.http import StreamingHttpResponse
@@ -44,3 +45,9 @@ class CaveView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_my_caves(request):
+    queryset = Cave.objects.filter(author=request.user.id)
+    serializer = CaveSerializer(queryset, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
