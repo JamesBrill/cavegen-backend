4162c44e5edae6b1cd12841414ca2c044e9159c9
James Brill <jebssnooker@hotmail.com>
2016-12-31 18:22:50 +0000

    Add UUID field to cave model

--- caves/models.py
+++ caves/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
 class Cave(models.Model):
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     text = models.TextField()
     author = models.ForeignKey(User, default=1)
