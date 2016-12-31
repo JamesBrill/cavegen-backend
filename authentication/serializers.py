8576d88875ee4b55c159c6dde1bdc39c694a63fd
James Brill <jebssnooker@hotmail.com>
2016-12-31 20:11:26 +0000

    Fix user serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('picture')
+        fields = ('picture',)
 
 class UserSerializer(serializers.ModelSerializer):
     userprofile = UserProfileSerializer()
