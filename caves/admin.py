113d1e5f54e792949a32761d2fe5ca1be5857461
James Brill <jebssnooker@hotmail.com>
2017-01-01 12:55:17 +0000

    Add cave admin

--- caves/admin.py
+++ caves/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cave
 
-# Register your models here.
+class CaveAdmin(admin.ModelAdmin):
+    fields = ['uuid', 'text', 'author']
+    list_display = ['uuid', 'text', 'author']
+    search_fields = ['author__email']
+
+admin.site.register(Cave, CaveAdmin)
