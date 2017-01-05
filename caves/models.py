cd9a73d33d2c31dbbeed6b5fafc21e716c5d45b2
James Brill <jebssnooker@hotmail.com>
2017-01-05 21:31:35 +0000

    Add date created field to cave model

--- caves/models.py
+++ caves/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class Cave(models.Model):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4)
@@ -8,6 +9,7 @@ class Cave(models.Model):
     text = models.TextField()
     author = models.ForeignKey(User, default=1)
     is_public = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
         if not self.name:
--- caves/serializers.py
+++ caves/serializers.py
@@ -4,4 +4,4 @@ from .models import Cave
 class CaveSerializer(ModelSerializer):
     class Meta:
         model = Cave
-        fields = ('id', 'name', 'uuid', 'text', 'author', 'is_public')
+        fields = ('id', 'name', 'uuid', 'text', 'author', 'is_public', 'date_created')
