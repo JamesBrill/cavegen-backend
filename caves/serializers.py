0cf91f107dbaa0f20738970f7af6790a2af38c5e
James Brill <jebssnooker@hotmail.com>
2017-01-07 12:09:49 +0000

    Include author name in serialized cave

--- caves/serializers.py
+++ caves/serializers.py
@@ -1,7 +1,8 @@
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework.serializers import ModelSerializer, SerializerMethodField, CharField
 from .models import Cave
 
 class CaveSerializer(ModelSerializer):
+    author_name = CharField(source='author.userprofile.display_name', read_only=True)
     class Meta:
         model = Cave
-        fields = ('id', 'name', 'uuid', 'text', 'author', 'is_public', 'date_created')
+        fields = ('id', 'name', 'uuid', 'text', 'author', 'is_public', 'date_created', 'author_name')
