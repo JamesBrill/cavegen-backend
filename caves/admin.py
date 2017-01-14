c37afc386337e50bbfb67c36c70603f6860a547c
James Brill <jebssnooker@hotmail.com>
2017-01-14 20:56:59 +0000

    Add author name column to caves admin

--- caves/admin.py
+++ caves/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import Cave
 
+
 class CaveAdmin(admin.ModelAdmin):
     fields = ['uuid', 'name', 'text', 'author', 'date_created']
-    list_display = ['uuid', 'name', 'text', 'author', 'date_created']
+    list_display = ['uuid', 'name', 'text', 'author', 'date_created', 'get_author_name']
+    readonly_fields = ('date_created',)
     search_fields = ['name', 'author__email']
+
+    def get_author_name(self, obj):
+        return obj.author.userprofile.display_name
+    get_author_name.admin_order_field = 'author'  # Allows column order sorting
+    get_author_name.short_description = 'Author Name'  # Renames column head
 
 admin.site.register(Cave, CaveAdmin)
