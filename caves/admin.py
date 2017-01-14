5175d895a7f40ad681617678b0d2f412476b60b6
James Brill <jebssnooker@hotmail.com>
2017-01-14 20:48:47 +0000

    Show cave creation date in admin

--- caves/admin.py
+++ caves/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Cave
 
 class CaveAdmin(admin.ModelAdmin):
-    fields = ['uuid', 'name', 'text', 'author']
-    list_display = ['uuid', 'name', 'text', 'author']
+    fields = ['uuid', 'name', 'text', 'author', 'date_created']
+    list_display = ['uuid', 'name', 'text', 'author', 'date_created']
     search_fields = ['name', 'author__email']
 
 admin.site.register(Cave, CaveAdmin)
